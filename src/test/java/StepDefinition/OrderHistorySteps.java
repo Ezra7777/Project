9a8d18af37e35053198c94d736ef90555c8b647c
EsraOzturk <esosh77@hotmail.com>
2020-03-30 16:49:05 -0400

    Merge branch 'master' of /Users/EsraOzturk/IdeaProjects/Project with conflicts.

--- src/test/java/StepDefinition/OrderHistorySteps.java
+++ src/test/java/StepDefinition/OrderHistorySteps.java
@@ -1,23 +1,8 @@
 package StepDefinition;
 
-import cucumber.api.java.en.Given;
 import pageObjectModel.AbstractClass;
-import pageObjectModel.OrderHistoryPage;
 
 public class OrderHistorySteps extends AbstractClass {
-
-    OrderHistoryPage orderHistoryPage= new OrderHistoryPage();
-
-    @Given("^click on your name$")
-    public void click_on_your_name() {
-
-    }
-
-    @Given("^click on order history and details$")
-    public void click_on_order_history_and_details() {
-
-    }
-
-
+    OrderHistory
 
 }
